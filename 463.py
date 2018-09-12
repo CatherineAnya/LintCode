7bcdaaf80bc2cad58f9270285a46d5ceaafb7edb
CatherineAnya <839067728@qq.com>
2018-09-12 13:39:41 +0800

    Create 463.py

---
+++ 463.py
@@ -0,0 +1,11 @@
+class Solution:
+    """
+    @param A: an integer array
+    @return: nothing
+    """
+    def sortIntegers(self, A):
+        # write your code here
+        A.sort()
+        
+
+哈哈哈哈读题后看到我代码的王先森有没有被我的机智震撼到
