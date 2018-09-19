415e2b718c4e3a2afb1d4fd0318c1b6a2e38899d
CatherineAnya <839067728@qq.com>
2018-09-19 21:07:38 +0800

    Update 632.二叉树的最大节点.py

--- Entry level/632.二叉树的最大节点.py
+++ Entry level/632.二叉树的最大节点.py
@@ -8,3 +8,25 @@
 # 知识点：
 # 1.
 # 2.
+# 3.
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+class Solution:
+    """
+    @param: root: the root of tree
+    @return: the max node
+    """
+    maxnum = -9999
+    node = None
+    def maxNode(self, root):
+        # write your code here
+        if root is None:
+            return None
+        if root.val > self.maxnum:
+            self.maxnum = root.val
+            self.node = root
+        self.maxNode(root.left)
+        self.maxNode(root.right)
+        return self.node
