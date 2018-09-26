f98bd53940b1643dfc85310e7db08729195ddde0
CatherineAnya <839067728@qq.com>
2018-09-26 14:41:03 +0800

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,3 +1,4 @@
+# 101ms
 class Solution:
     """
     @param A: sorted integer array A
@@ -13,3 +14,19 @@ class Solution:
             newArray.append(j)
         newArray.sort()
         return newArray
+    
+# 105ms
+class Solution:
+    """
+    @param A: sorted integer array A
+    @param B: sorted integer array B
+    @return: A new sorted integer array
+    """
+    def mergeSortedArray(self, A, B):
+        # write your code here
+        for j in B:
+            A.append(j)
+        A.sort()
+        return A
+    
+为什么还慢了呢 =.= :=( ^(..)^
