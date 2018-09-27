4bb9041d9d9ab809b05aac6e6bc883e8df173c25
CatherineAnya <839067728@qq.com>
2018-09-27 17:21:34 +0800

    Update 82.py

--- 82.py
+++ 82.py
@@ -17,3 +17,10 @@ class Solution:
                 A.pop(i)
                 A.pop(j-1)
                 j = 1
+if __name__ == '__main__':
+    N = int(input('Enter an integer:'))
+    intArray = list()
+    for i in range(2*N+1):
+        intArray.append(input('Enter a integer:'))
+    s = Solution()
+    print(s.singleNumber(intArray))
