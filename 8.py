b3a74f835a7af373de18933825f64e0b5ca441fb
CatherineAnya <839067728@qq.com>
2018-09-27 15:05:30 +0800

    Update 8.py

--- 8.py
+++ 8.py
@@ -15,10 +15,33 @@ class Solution:
         newStr = str[:(length - offset):]
         return offsetStr + newStr
 if __name__ == '__main__':
-    string = input('Enter a string:')
+    N = int(input('Enter an integer:'))
+    charArray = list()
+    for i in range(N):
+        charArray.append(input('Enter a char:'))
     o = int(input('Enter offset:'))
     s = Solution()
-    print(s.rotateString(string, o))
-    
-LintCode 编辑器竟然不支持切片操作 :=(
-再想想
+    print(s.rotateString(charArray, o))
+
+# 701ms
+class Solution:
+    """
+    @param str: An array of char
+    @param offset: An integer
+    @return: nothing
+    """
+    def rotateString(self, str, offset):
+        # write your code here
+        length = len(str)
+        if length == 0:
+            return str
+        offset = offset % length
+        value = length - offset
+        if offset == 0:
+            return str
+        list1 = str[:value]
+        list2 = str[value:]
+        del str[:]
+        str.extend(list2)
+        str.extend(list1)
+        return str
