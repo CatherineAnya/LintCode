be9499c24c0feeb8858927a128f60b9f929fdce8
CatherineAnya <839067728@qq.com>
2018-10-08 17:20:07 +0800

    Create 1285.Power of Four.py

---
+++ 1285.Power of Four.py
@@ -0,0 +1,44 @@
+# Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+# 样例
+# Example1:
+# Given num = 16, 
+# return true.
+
+# Example2:
+# Given num = 5, 
+# return false.
+
+# 挑战
+# Could you solve it without loops/recursion?
+class Solution:
+    """
+    @param num: an integer
+    @return: whether the integer is a power of 4
+    """
+    def isPowerOfFour(self, num):
+        # Write your code here
+        if num == 1:
+            return True
+        while num > 1:
+            num = num / 4
+        if num == 1:
+            return True
+        else:
+            return False
+# 不使用循环
+import math
+class Solution:
+    """
+    @param num: an integer
+    @return: whether the integer is a power of 4
+    """
+    def isPowerOfFour(self, num):
+        # Write your code here
+        if num <= 0:
+            return False
+        num = math.log10(num) / math.log10(4)
+        if num % 1 == 0:
+            return True
+        else:
+            return False
